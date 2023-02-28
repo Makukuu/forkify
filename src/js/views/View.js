7850aac4e64ef657b95b9379606e10319589ce59
Martin Kukuruzar <martinkukuruzar@gmail.com>
2023-02-28 11:16:44 +0100

    Implement bookmark functionality

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -25,7 +25,6 @@ export default class View {
 
     newElements.forEach((newEl, i) => {
       const curEl = curElements[i];
-      console.log(curEl, newEl.isEqualNode(curEl));
 
       // Updates changed TEXT
       if (
