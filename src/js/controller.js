0e3ae3563c21cc40d4944eaf50069358f0dc99b6
Martin Kukuruzar <martinkukuruzar@gmail.com>
2023-02-28 10:35:18 +0100

    Reset page after a new search

--- src/js/controller.js
+++ src/js/controller.js
@@ -28,6 +28,9 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
+    // 0) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
     // Loading recipe
     // because loadRecipe is an async function and an async function
     // always returns a promise that we need to handle if we want a
@@ -77,7 +80,8 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
